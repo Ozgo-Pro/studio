b99bd17d3f70a3919f8359155ac1e2b90887033f
Augustin Galatanu (Ozgo) <Augustin.Galatanu@gmail.com>
2025-10-21 09:49:28 +0000

    increase the file limit to 25MB

--- src/components/image-uploader.tsx
+++ src/components/image-uploader.tsx
@@ -16,7 +16,7 @@ interface ImageUploaderProps {
   id: string;
 }
 
-const MAX_FILE_SIZE_MB = 2;
+const MAX_FILE_SIZE_MB = 25;
 const MAX_FILE_SIZE_BYTES = MAX_FILE_SIZE_MB * 1024 * 1024;
 
 export function ImageUploader({
