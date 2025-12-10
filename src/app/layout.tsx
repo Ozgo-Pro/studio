03e6dea55f4377da9c5ee1bca727eb4878d95380
Augustin Galatanu (Ozgo) <Augustin.Galatanu@gmail.com>
2025-12-10 14:20:01 +0000

    <!-- Google tag (gtag.js) --> <script async src="https://www.googletagma

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 
@@ -25,6 +26,19 @@ export default function RootLayout({
       <body className="font-body antialiased">
         {children}
         <Toaster />
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-9CWKZGTCRK"
+        ></Script>
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-9CWKZGTCRK');
+          `}
+        </Script>
       </body>
     </html>
   );
